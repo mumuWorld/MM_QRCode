0397627556810443148a2c9e86003e2f337c2ec2
yangjie <yangjie@cangbaopai.com>
2019-08-02 19:14:19 +0800

    【进度条】

--- MMQRCode/MMQRCode/Class/Component/WebVC/MQWKWebCV.swift
+++ MMQRCode/MMQRCode/Class/Component/WebVC/MQWKWebCV.swift
@@ -21,6 +21,11 @@ class MQWKWebCV: MQBaseViewController {
         label.alpha = 0;
         return label
     }()
+    lazy var progressView: UIView = {
+        let progress = UIView(frame: CGRect(x: 0, y: MQNavigationBarHeight, width: 0, height: 2))
+        progress.backgroundColor = MQMainColor
+        return progress
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
@@ -28,7 +33,9 @@ class MQWKWebCV: MQBaseViewController {
         loadingUrl()
     }
     deinit {
-        _wkWebView?.removeObserver(self, forKeyPath: "contentOffset")
+        _wkWebView?.scrollView.removeObserver(self, forKeyPath: "contentOffset")
+        _wkWebView?.removeObserver(self, forKeyPath: "estimatedProgress")
+
     }
 }
 
@@ -41,7 +48,7 @@ extension MQWKWebCV: MQViewLoadSubViewProtocol {
         _wkWebView?.backgroundColor = UIColor.init(white: 0.0, alpha: 0.0)
 //        _wkWebView?.scrollView.backgroundColor = UIColor.init(white: 0.0, alpha: 0.0)
         _wkWebView?.scrollView.addObserver(self, forKeyPath: "contentOffset", options: [.new], context: nil)
-        _wkWebView?.scrollView.addObserver(self, forKeyPath: "estimatedProgress", options: [.new], context: nil)
+        _wkWebView?.addObserver(self, forKeyPath: "estimatedProgress", options: [.new], context: nil)
 
         view.addSubview(_wkWebView!)
         
@@ -55,6 +62,8 @@ extension MQWKWebCV: MQViewLoadSubViewProtocol {
         self.supportLabel.sizeToFit()
         supportLabel.mm_centerX = MQScreenWidth * 0.5
         view.insertSubview(supportLabel, at: 0)
+        
+        view.addSubview(progressView)
     }
 }
 extension MQWKWebCV {
@@ -78,14 +87,14 @@ extension MQWKWebCV {
             
             if  fitValue < 0 {
                 let alpah = -fitValue / 60
-                MQPrintLog(message: "alpha=\(alpah)")
                 supportLabel.alpha = min(alpah, 1)
             } else {
                 supportLabel.alpha = 0
             }
-        } else if keyPath == "estimatedProgress" {
-            let newValue = changes[NSKeyValueChangeKey.newKey] as? Double ?? 0
-            
+        } else if keyPath == "estimatedProgress" { //进度条
+            let newValue = changes[NSKeyValueChangeKey.newKey] as? CGFloat ?? 0
+            let fitValue = newValue * MQScreenWidth
+            progressView.mm_width = min(fitValue, MQScreenWidth)
         }
     }
 }
@@ -94,12 +103,18 @@ extension MQWKWebCV: WKUIDelegate {
 }
 extension MQWKWebCV: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        progressView.isHidden = false
+        progressView.mm_width = 0
         MQPrintLog(message: "startLoading")
     }
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         decisionHandler(WKNavigationActionPolicy.allow)
     }
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        let deadline = DispatchTime.now() + 0.5
+        DispatchQueue.main.asyncAfter(deadline: deadline, execute: {
+            self.progressView.isHidden = true
+        })
         if let title = webView.title {
             self.navigationItem.title = title
         }
